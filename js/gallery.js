49c0f2ca40fe41691bf66a2de8bf342a67d3750b
octobe <octoberw@gmail.com>
2023-12-04 13:17:07 +0800

    Update gallery.js
    add setImage function

--- js/gallery.js
+++ js/gallery.js
@@ -11,3 +11,22 @@ function unDo(){
     document.getElementById("image").style.backgroundImage = '';
     document.getElementById("image").innerHTML = 'Hover over an image below to display here.';		
       }
+
+function setImage() {
+  var images = document.querySelectorAll('.preview');
+  for (let i = 0; i < images.length; i++) {
+    let attributes = {
+      'tabIndex': 0,
+      'onmouseover': 'upDate(this)',
+      'onmouseout': 'unDo()',
+      'onfocus': 'upDate(this)',
+      'onblur': 'unDo()'
+    };
+
+    for (let attr in attributes) {
+      images[i].setAttribute(attr, attributes[attr]);
+    }
+  }
+}
+      
+      
